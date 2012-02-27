33f75adc931c2ee5aba2ddb4669b857c182237b3
Jace Bennett <jace.bennett@gmail.com>
2012-02-27 16:57:27 -0500

    Skeletal (ugly) controllers and views

---
+++ Web/Controllers/SessionsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web.Mvc;
+using Raven.Client;
+using Web.Models;
+
+namespace Web.Controllers
+{
+	public class SessionsController : Controller
+	{
+		//
+		// GET: /Sessions/Details/5
+
+		public ActionResult Details(string id)
+		{
+			var session = DocumentSession.Load<Session>(id);
+			return View(session);
+		}
+
+		//
+		// GET: /Sessions/Create
+
+		public ActionResult Create(string studyId)
+		{
+			var session = new Session(studyId, String.Empty);
+			return View(session);
+		} 
+
+		//
+		// POST: /Sessions/Create
+
+		[HttpPost]
+		public ActionResult Create(string studyId, string name)
+		{
+			var session = new Session(studyId, name);
+			try
+			{
+				DocumentSession.Store(session);
+				DocumentSession.SaveChanges();
+
+				return RedirectToAction("Details", "Studies", new { Id = studyId });
+			}
+			catch
+			{
+				return View(session);
+			}
+		}
+
+		private IDocumentSession _documentSession;
+		protected IDocumentSession DocumentSession
+		{
+			get
+			{
+				if (_documentSession == null)
+				{
+					_documentSession = MvcApplication.Store.OpenSession();
+				}
+				return _documentSession;
+			}
+		}
+	}
+}
---
+++ Web/Controllers/StudiesController.cs
@@ -0,0 +1,73 @@
+﻿using System.Web.Mvc;
+using Raven.Client;
+using Raven.Client.Linq;
+using System.Linq;
+using Web.Models;
+
+namespace Web.Controllers
+{
+	public class StudiesController : Controller
+	{
+		//
+		// GET: /Studies/
+
+		public ActionResult Index()
+		{
+			var results = DocumentSession.Query<Study>();
+			return View(results);
+		}
+
+		//
+		// GET: /Studies/Details/5
+
+		public ActionResult Details(string id)
+		{
+			var study = DocumentSession.Load<Study>(id);
+			ViewBag.Sessions = DocumentSession.Query<Session>()
+									   .Where(x => x.StudyId == id)
+									   .ToList();
+			return View(study);
+		}
+
+		//
+		// GET: /Studies/Create
+
+		public ActionResult Create()
+		{
+			return View(new Study());
+		} 
+
+		//
+		// POST: /Studies/Create
+
+		[HttpPost]
+		public ActionResult Create(string name)
+		{
+			var study = new Study { Name = name };
+			try
+			{
+				DocumentSession.Store(study);
+				DocumentSession.SaveChanges();
+
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				return View(study);
+			}
+		}
+
+		private IDocumentSession _documentSession;
+		protected IDocumentSession DocumentSession
+		{
+			get
+			{
+				if (_documentSession == null)
+				{
+					_documentSession = MvcApplication.Store.OpenSession();
+				}
+				return _documentSession;
+			}
+		}
+	}
+}
