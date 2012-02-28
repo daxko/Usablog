d4356d4821f6e7b71eefe755f1cc21159b527fad
Patrick Boudreaux <pboudreaux@daxko.com>
2012-02-28 10:33:17 -0500

    studies list as home page

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -25,7 +25,7 @@ namespace Web
 			routes.MapRoute(
 				"Default", // Route name
 				"{controller}/{action}/{*id}", // URL with parameters
-				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+				new { controller = "Studies", action = "Index", id = UrlParameter.Optional } // Parameter defaults
 			);
 
 		}
