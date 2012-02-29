e37ebb35907b5eeeea2904813230548778209922
Jace Bennett <jace.bennett@gmail.com>
2012-02-28 20:30:22 -0500

    Usablog link works. Logged in as shows.

--- Web/Scripts/site.js
+++ Web/Scripts/site.js
@@ -1,27 +1,30 @@
 ﻿(function ($) {
 	$(function () {
 		$("button.btn-link[href]").click(function () {
-			var url = $(this).attr("href");
-			window.location = url;
+			window.location = $(this).attr("href");
+		});
+
+		$("[href=#]").click(function(ev) {
+			return false;
 		});
 	});
 
 	var tmplConfig = {
 		suffix: "tmpl",
 		plugin: "jquery/view/tmpl",
-		renderer: function(id, text) {
-			return function(data) {
+		renderer: function (id, text) {
+			return function (data) {
 				return $.tmpl(text, data);
 			};
 		},
-		script: function(id, text) {
+		script: function (id, text) {
 			var tmpl = $.tmpl(text).toString();
 			return "function(data){return (" +
 				tmpl +
 					").call(jQuery, jQuery, data); }";
 		}
 	};
-	
+
 
 	// $.View.register(tmplConfig); // no-op function in micro jmvc :(
 	$.View.types[".tmpl"] = tmplConfig;
