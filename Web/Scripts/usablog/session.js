0d235271f610f4ca3d637d3668a071437436337a
Patrick Boudreaux <pboudreaux@daxko.com>
2012-02-29 09:53:45 -0500

    first pass of log entry form

--- Web/Scripts/usablog/session.js
+++ Web/Scripts/usablog/session.js
@@ -20,8 +20,14 @@ $.Controller('SessionController', {
 		});
 		
 		this.log = $("<ol></ol>").addClass("log");
+		this.entryArea = $("<div></div>");
 		this.element.append(this.log);
-		
+		this.element.append(this.entryArea);
+
+		var controller = this;
+		$.View("//scripts/usablog/sessionentryform.tmpl", {}, function (result) {
+			controller.entryArea.append(result);
+		});
 	},
 
 	logEntryAdded: function(item) {
@@ -31,9 +37,25 @@ $.Controller('SessionController', {
 		});
 	},
 
-	addLogEntry: function(value) {
-		var entry = new LogEntry({ value: value, timeStamp:new Date()});
+	addLogEntry: function(value, timeStamp) {
+		var entry = new LogEntry({ value: value, timeStamp:timeStamp});
 		
 		this.logEntries.push(entry);
+	},
+
+	"input[name=logEntry] keyup": function (el, ev) {
+		this.entryTimeStamp = this.entryTimeStamp || new Date();
+		this.entry = $(ev.currentTarget).val();
+	},
+
+	"form submit": function (el, ev) {
+		ev.preventDefault();
+		if(!this.entry)
+			return;
+		
+		this.addLogEntry(this.entry, this.entryTimeStamp);
+
+		this.entryTimeStamp = null;
+		this.entry = null;
 	}
 });
