c33852ed6c0445fba92144de6f0483fa33f0aa28
Patrick Boudreaux <pboudreaux@daxko.com>
2012-02-29 14:18:47 -0500

    extracted EntryInputController

--- Web/Scripts/usablog/session.js
+++ Web/Scripts/usablog/session.js
@@ -24,19 +24,16 @@ $.Controller('Usablog.SessionController', {
 
 		var self = this;
 		this.logEntries.bind("add", function(ev, newItems) {
-			for(var i in newItems)
+			for (var i in newItems)
 				self.logEntryAdded(newItems[i]);
 		});
-		
+
 		this.log = $("<ol></ol>").addClass("log");
-		this.entryArea = $("<div></div>");
 		this.element.append(this.log);
+		this.entryArea = $("<div></div>");
 		this.element.append(this.entryArea);
 
-		var controller = this;
-		$.View("//scripts/usablog/sessionentryform.tmpl", {}, function (result) {
-			controller.entryArea.append(result);
-		});
+		new Usablog.EntryInputController(this.entryArea, {model: this.logEntries});
 	},
 
 	logEntryAdded: function(item) {
@@ -50,6 +47,24 @@ $.Controller('Usablog.SessionController', {
 		var entry = new LogEntry({ value: value, timeStamp:timeStamp});
 		
 		this.logEntries.push(entry);
+	}
+});
+
+$.Controller('Usablog.EntryInputController', {
+	
+	init:function (raw_el, opts) {
+
+		this.model = opts.model;
+		var controller = this;
+		$.View("//scripts/usablog/sessionentryform.tmpl", {}, function (result) {
+			controller.element.append(result);
+		});
+	},
+	
+	addLogEntry: function(value, timeStamp) {
+		var entry = new Usablog.LogEntry({ value: value, timeStamp:timeStamp});
+		
+		this.model.push(entry);
 	},
 
 	"input[name=logEntry] keyup": function (el, ev) {
